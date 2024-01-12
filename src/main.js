b7884b209ffbcaf51c152e11743aaa4feda78d3a
torreslucs23 <torreslucs23@gmail.com>
2024-01-12 17:18:19 -0300

    added base button component

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import App from './App.vue';
 
 import BaseCard from './components/UI/BaseCard.vue';
 
+import BaseButton from './components/UI/BaseButton.vue';
+
 const app = createApp(App);
 
 app.component('base-card', BaseCard);
+
+app.component('base-button', BaseButton);
 
 app.mount('#app');
